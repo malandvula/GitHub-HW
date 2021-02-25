0aa032e87593247579dac4d8e287282ba9809fd5
mq mala <malandula@icloud.com>
2021-02-24 23:14:57 -0600

    Completed practicing the basics

--- Class-3-tutorial/test/com/virtualpairprogrammer/isbntools/ValidateISBNTest.java
+++ Class-3-tutorial/test/com/virtualpairprogrammer/isbntools/ValidateISBNTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkAValidISBN() {
+	public void checkAValid10DigitISBN() {
 		ISBNValidator validator = new ISBNValidator();
 		boolean result = validator.checkISBN("0140449116");
 		assertTrue("First value", result);
@@ -16,9 +16,32 @@ public class ValidateISBNTest {
 	}
 	
 	@Test
-	public void checkAnInvalidISBN() {
+	public void checkAValid13DigitISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("9780345434869");
+		assertTrue("First value", result);
+		result = validator.checkISBN("9781938895302");
+		assertTrue("Second value", result);
+	}
+	
+	@Test
+	public void tenDigitISBNNumbersEndingInAnXAreValid() {
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("012000030X");
+		assertTrue(result);
+	}
+	
+	@Test
+	public void checkAnInvalid10DigitISBN() {
 		ISBNValidator validator = new ISBNValidator();
 		boolean result = validator.checkISBN("0140449117");
+		assertFalse(result);
+	}
+	
+	@Test
+	public void checkAnInvalid13DigitISBN() {
+		ISBNValidator validator = new ISBNValidator();
+		boolean result = validator.checkISBN("9781938895306");
 		assertFalse(result);
 	}
 	
